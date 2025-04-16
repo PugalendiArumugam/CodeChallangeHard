1e39f385065d76f069617f3ed4f5efabe5727eee
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-04-16 22:13:26 +0530

    Add files via upload

---
+++ SortCharacterByFreq.java
@@ -0,0 +1,18 @@
+package LeetCode.Medium;
+
+public class SortCharacterByFreq {
+    public static void main(String[] args) {
+        SortCharacterByFreq scbf = new SortCharacterByFreq();
+        System.out.println(scbf.frequencySort("tree"));
+        System.out.println(scbf.frequencySort("cccaaa"));
+//        System.out.println(scbf.frequencySort("Aabb"));
+//        System.out.println(scbf.frequencySort("RILmT0oZF8"));
+//        System.out.println(scbf.frequencySort("ApsY9AQF2xsz1pbHl5Po7Q1yKNOthVVY9wkGrvktI1dcbXirB5k82sK1lFrx2Wv3mc1viEfvt31TRor6HFCe4M40hztHX3pVSAoi"));
+    }
+
+
+    public String frequencySort(String s) {
+
+        return "";
+    }
+}
